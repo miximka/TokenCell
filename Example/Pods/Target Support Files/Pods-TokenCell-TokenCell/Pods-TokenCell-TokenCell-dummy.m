437aae925e467d6868dfa39d416222e2da93c06c
miximka <miximka@gmail.com>
2016-02-17 15:55:43 +0100

    Bump version

--- Example/Pods/Target Support Files/Pods-TokenCell-TokenCell/Pods-TokenCell-TokenCell-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_TokenCell_TokenCell : NSObject
-@end
-@implementation PodsDummy_Pods_TokenCell_TokenCell
-@end
---
+++ Example/Pods/Target Support Files/TokenCell/TokenCell-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_TokenCell : NSObject
+@end
+@implementation PodsDummy_TokenCell
+@end
